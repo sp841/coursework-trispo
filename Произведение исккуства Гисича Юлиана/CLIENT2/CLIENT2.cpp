1d97aace125de0e6170f19ac1825c9730828d1fc
GreatestCaesar <yulik.gisic.re@gmail.com>
2020-04-16 15:26:56 +0300

    add work

---
+++ Произведение исккуства Гисича Юлиана/CLIENT2/CLIENT2.cpp
@@ -0,0 +1,57 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <iomanip>
+#include <cstdlib>
+
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+SOCKET Connection;
+
+void ClientHandler() {
+	char msg[256];
+	while (true) {
+		recv(Connection, msg, sizeof(msg), NULL);
+		cout << msg << endl;
+	}
+}
+
+
+int main(int argc, char* argv[]) {
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL);
+
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+		cout << "Error: failed connect to server.\n";
+		return 1;
+	}
+	cout << "Connected!\n";
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
+
+	char msg1[256];
+	while (true) {
+		cin.getline(msg1, sizeof(msg1));
+		send(Connection, msg1, sizeof(msg1), NULL);
+		Sleep(10);
+	}
+	system("pause");
+	return 0;
+}
