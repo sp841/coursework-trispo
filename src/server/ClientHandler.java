5dcded8fb1636acdd9e7acf95c07c8fb3a92b8b0
igorvolchek <ivauchok@yandex.by>
2020-04-23 13:31:25 +0300

    add advanced version

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,5 +1,5 @@
 package server;
-
+import java.util.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -8,6 +8,8 @@ import java.util.Scanner;
 
 // реализуем интерфейс Runnable, который позволяет работать с потоками
 public class ClientHandler implements Runnable {
+
+
     // экземпляр нашего сервера
     private Server server;
     // исходящее сообщение
@@ -37,10 +39,14 @@ public class ClientHandler implements Runnable {
     // мы вызываем new Thread(client).start();
     @Override
     public void run() {
+        Map<String, String> termins = new HashMap<String, String>();
+        termins.put("телефон", "Система технических приспособлений для передачи звуков на расстояние по проводам при помощи электрической энергии.");
+        termins.put("компьютер", "Электронно-вычислительная машина (ЭВМ).");
+        int n;
         try {
             while (true) {
                 // сервер отправляет сообщение
-                server.sendMessageToAllClients("Новый участник вошёл в чат!");
+
                 server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
                 break;
             }
@@ -49,15 +55,21 @@ public class ClientHandler implements Runnable {
                 // Если от клиента пришло сообщение
                 if (inMessage.hasNext()) {
                     String clientMessage = inMessage.nextLine();
+                    String clientMessage1="";
+                    if(clientMessage.equalsIgnoreCase("телефон"))clientMessage1="Телефон - Система технических приспособлений для передачи звуков на расстояние по проводам при помощи электрической энергии.";
+                    if(clientMessage.equalsIgnoreCase("компьютер"))  clientMessage1="Компьютер - Электронно-вычислительная машина (ЭВМ).";
+                    if(clientMessage.equalsIgnoreCase("микрофон"))  clientMessage1="Микрофон -Прибор, преобразующий звуковые колебания в электрические для передачи звуков на расстояние.";
+                    if(clientMessage.equalsIgnoreCase("наушники"))  clientMessage1="Наушники - два телефона с оголовьем, предназначенные для подключения к бытовым радиоэлектронным аппаратам.";
                     // если клиент отправляет данное сообщение, то цикл прерывается и
                     // клиент выходит из чата
                     if (clientMessage.equalsIgnoreCase("##session##end##")) {
                         break;
                     }
                     // выводим в консоль сообщение (для теста)
-                    System.out.println(clientMessage);
+
                     // отправляем данное сообщение всем клиентам
-                    server.sendMessageToAllClients(clientMessage);
+                    System.out.println(clientMessage1);
+                    server.sendMessageToAllClients(clientMessage1);
                 }
                 // останавливаем выполнение потока на 100 мс
                 Thread.sleep(100);
